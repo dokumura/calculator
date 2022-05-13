d29020f83aebade1dc123547a6636f8db6cc51a7
dokumura <61328746+dokumura@users.noreply.github.com>
2022-05-13 15:11:22 +0200

    Add test to OperationException (#2)
    Co-authored-by: ddob <ddob@ESPC012296.gft.com>

--- src/test/java/com/poc/calculator/services/OperationServiceImplTest.java
+++ src/test/java/com/poc/calculator/services/OperationServiceImplTest.java
@@ -6,6 +6,9 @@ import java.math.BigDecimal;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import com.poc.calculator.exceptions.OperationException;
 import com.poc.calculator.models.OperationBO;
 import com.poc.calculator.responses.ResultResponse;
 import com.poc.calculator.services.impl.OperationServiceImpl;
@@ -26,6 +29,15 @@ public class OperationServiceImplTest {
 	public void givenOperationBO_whenServiceAddition_thenVerifyResultResponse() {
 		ResultResponse resultResponse = service.getAddition(operationBO);
 	    assertEquals(resultResponse.getResult(),new BigDecimal(3));
+	}
+	
+	@Test
+	public void givenOperationBOExceedsLimit_whenServiceAddition_thenVerifyException() {
+		operationBO.setOperator1(new BigDecimal(1000000000));
+		OperationException exception = Assertions.assertThrows(OperationException.class, () -> {
+			service.getAddition(operationBO);
+	  });
+	  Assertions.assertEquals("Result exceeds the limit", exception.getMessage());
 	}
 	
 	@Test
